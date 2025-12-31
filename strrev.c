7f45dd3d6591ca591a38b5f7590551311c1288a0
5603basvapuramhasini <5603basvapuramhasini@gmail.com>
2025-12-31 19:44:03 +0530

    Add files via upload

---
+++ strrev.c
@@ -0,0 +1,10 @@
+//String length function
+//strrev(char[]);
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char str[] ="Hyderabad";
+	strrev(str);
+	printf("\nReversed String is: %s", str);
+}
