3412c10ea1631dd532fb396f274ac00d10fa079d
5603basvapuramhasini <5603basvapuramhasini@gmail.com>
2025-12-31 19:48:41 +0530

    Add files via upload

---
+++ manpliation.c
@@ -0,0 +1,13 @@
+//String length function
+//int strlen(char[]);
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char str[] ="Hyderabad";
+	int len, size;
+	len = strlen(str);
+	size = sizeof(str);
+	printf("\nlength of the given string is: %d", len);
+	printf("\nsize of the given string is: %d", size);
+}
