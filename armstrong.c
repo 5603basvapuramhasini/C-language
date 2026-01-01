ec6ac47c0808f3e01da1cd697e17a967a332c141
5603basvapuramhasini <5603basvapuramhasini@gmail.com>
2026-01-01 11:05:12 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	int num, rem, sum=0, count=0, temp;
+	printf("Enter any Number:");
+	scanf("%d",&num);
+	temp = num;
+	//count the digits
+	while(num>0)
+	{
+		count++; //count=count+1
+		num=num/10;
+	}
+	num=temp;
+	//Powered sum
+	while(num>0)
+	{
+		rem = num%10;
+		sum = sum+pow(rem,count);
+		num=num/10;
+	}
+	if(temp==sum)
+	     printf("%d is an Armstrong Number",temp);
+	else
+	     printf("%d is not an Armstrong Number",temp);
+}
